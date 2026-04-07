ab1d3d8a9ce71986883ea3d349e5f3691e0c1bd7
Valentin-Garcia-T <v.garcatorrejn@uandresbello.edu>
2026-04-07 02:59:18 -0400

    Detallitos

--- Assets/_Scripts/empezarEscena.cs
+++ Assets/_Scripts/empezarEscena.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class empezarEscena : MonoBehaviour
 {
+    public GameObject[] objetos;
+    public GameObject Iniciar, FadeIn;
+    public GameObject player, inicial;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            FadeIn.SetActive(true);
+            Invoke("Empezar", 3f);
+        }
+    }
+
+    public void Empezar()
+    {
+        Iniciar.SetActive(false);
+        player.transform.position = inicial.transform.position;
+        foreach (GameObject obj in objetos)
+        {
+            obj.SetActive(false);
+        }
+
+        int indice = Random.Range(0, objetos.Length);
+
+        objetos[indice].SetActive(true);
     }
 }

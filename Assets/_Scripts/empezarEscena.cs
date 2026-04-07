d6d30d6c9a58adffa1127e9d97b370e9262bb959
Valentin-Garcia-T <v.garcatorrejn@uandresbello.edu>
2026-04-07 13:09:17 -0400

    Anim + Inicio

--- Assets/_Scripts/empezarEscena.cs
+++ Assets/_Scripts/empezarEscena.cs
@@ -6,16 +6,19 @@ public class empezarEscena : MonoBehaviour
 {
     public GameObject[] objetos;
     public GameObject Iniciar, FadeIn;
-    public GameObject player, inicial;
+    public GameObject player, inicial, previa;
 
     // Start is called before the first frame update
     void Start()
     {
+        player.transform.position = previa.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        FadeIn.transform.rotation = Quaternion.Euler(0, 0, 0);
+        Iniciar.transform.rotation = Quaternion.Euler(0, 0, 0);
         if (Input.GetKeyDown(KeyCode.Space))
         {
             FadeIn.SetActive(true);
@@ -25,7 +28,8 @@ public class empezarEscena : MonoBehaviour
 
     public void Empezar()
     {
-        Iniciar.SetActive(false);
+        Iniciar.SetActive(true);
+        FadeIn.SetActive(false);
         player.transform.position = inicial.transform.position;
         foreach (GameObject obj in objetos)
         {

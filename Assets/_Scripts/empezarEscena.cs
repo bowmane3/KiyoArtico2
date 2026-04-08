92ea025b4872db590e009d121e827674fd3fff11
Valentin-Garcia-T <v.garcatorrejn@uandresbello.edu>
2026-04-07 21:23:56 -0400

    Prueba

--- Assets/_Scripts/empezarEscena.cs
+++ Assets/_Scripts/empezarEscena.cs
@@ -1,29 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class empezarEscena : MonoBehaviour
 {
     public GameObject[] objetos;
     public GameObject Iniciar, FadeIn;
     public GameObject player, inicial, previa;
+    public InputActionReference inputActionReference;
+    InputAction action;
 
     // Start is called before the first frame update
     void Start()
     {
         player.transform.position = previa.transform.position;
+        action = inputActionReference.action;
+        // activar input !!!!!
+        action.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
+        bool hold = action.IsPressed();
+        bool pressed = action.WasPressedThisFrame();
+        bool release = action.WasReleasedThisFrame();
         FadeIn.transform.rotation = Quaternion.Euler(0, 0, 0);
         Iniciar.transform.rotation = Quaternion.Euler(0, 0, 0);
-        if (Input.GetKeyDown(KeyCode.Space))
+        
+        if (pressed)
         {
             FadeIn.SetActive(true);
             Invoke("Empezar", 3f);
         }
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    FadeIn.SetActive(true);
+        //    Invoke("Empezar", 3f);
+        //}
     }
 
     public void Empezar()
